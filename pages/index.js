e2bd7b3d8c8f6ce8a7095bcfab2316047feb0dfb
Володя <chernyx.vova2@gmail.com>
2023-07-09 22:43:57 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout ';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/data';
+
+export default function Home({ allPostsData }) {
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>Привет, меня зовут Володя и я учусь работать с Next.js</p>
+				<p>(Это пример веб-сайта. Вы создадите такой сайт на {' '}
+					<a href="https://nextjs.org/learn">наш учебник по Next.js</a>.)
+				</p>
+			</section>
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Блог</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							<Link href={`/posts/${id}`}>{title}</Link>
+							<br />
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
+}
+
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Layout from "../../components/layout ";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Data from "../../components/data";
+
+export default function Post({ postData }) {
+	return (
+		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			{postData.title}
+			<br />
+			{postData.id}
+			<br />
+			<Data dateString={postData.date} />
+			<br />
+			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+		</Layout>
+	);
+}
+
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps({ params }) {
+	const postData = await getPostData(params.id);
+	return {
+		props: {
+			postData,
+		},
+	};
+}
+
+
+
